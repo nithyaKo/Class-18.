027a131fd0600d08db639af8c0bff5b63418e66e
nithyaKo <86623961+nithyaKo@users.noreply.github.com>
2021-07-20 14:44:39 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var s1
+var s2
+
+function setup() {
+  createCanvas(400,400);
+
+  s1 = new Student("Jessica",11,6)
+  s1.display()
+
+  s2 = new Student("Alex",13,8)
+  s2.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+ 
+}
